171013ab60f2968809f3f46b380c78802ad87d15
Nahuel Grenikoff <60801014+nahuelGrenikoff@users.noreply.github.com>
2020-02-08 01:31:40 -0300

    Add files via upload

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,8 @@
 function mostrar()
 {
+    var precio = prompt("Ingrese el precio de lista");
+    var porcentaje = prompt("Ingrese el porcentaje de descuento");
 
+    var resultado = precio-(precio/100*porcentaje);
+    document.getElementById("elPrecioFinal").value = resultado;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,10 @@
 
 function mostrar()
 {
-
+    var num1 = prompt("Ingrese la altura del rectángulo");
+    var num2 = prompt("Ingrese el largo del rectángulo");
+    num1 = parseInt(num1);
+    num2 = parseInt(num2);
+    var resultado = num1*2+num2*2;
+    alert("El perímetro del rectángulo será: " + resultado);
 }
